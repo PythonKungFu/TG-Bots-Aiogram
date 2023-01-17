26ee72e5ac155182d5052cb923f50d7b42dde491
PythonKungFu <taurus700@yandex.ru>
2023-01-17 13:27:47 +0300

    6.1.2 Add publics API

--- 6 Telegram Bot Api/6.1.2 API example.py
+++ 6 Telegram Bot Api/6.1.2 API example.py
@@ -5,3 +5,6 @@ import requests
 url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/btc.json'
 response = requests.get(url).json()
 print(response['btc']['rub'])
+
+# Link publics API
+# https://github.com/public-apis/public-apis
