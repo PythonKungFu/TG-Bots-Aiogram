b6689550825bd840f4036c51dcfc9cd221e74dd6
PythonKungFu <taurus700@yandex.ru>
2023-01-17 13:22:34 +0300

    6.1.2 Api example

---
+++ 6 Telegram Bot Api/6.1.2 API example.py
@@ -0,0 +1,7 @@
+import requests
+
+
+# Simple example Api
+url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/btc.json'
+response = requests.get(url).json()
+print(response['btc']['rub'])
